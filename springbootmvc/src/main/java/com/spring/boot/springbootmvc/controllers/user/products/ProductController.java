c1f7fa2d46fb97bb72eaac47d6289a471d2d4bfe
amitKmrPa <amit.kmr.paswan995@gmail.com>
2022-12-04 23:20:41 +0530

    added api call for react-redux app

--- springbootmvc/src/main/java/com/spring/boot/springbootmvc/controllers/user/products/ProductController.java
+++ springbootmvc/src/main/java/com/spring/boot/springbootmvc/controllers/user/products/ProductController.java
@@ -6,13 +6,14 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.boot.springbootmvc.controllers.user.kart.KartService;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class ProductController {
     @Autowired
@@ -31,6 +32,24 @@ public class ProductController {
             modelAndView.addObject("userId", httpSession.getAttribute("userId"));
             modelAndView.setViewName("product/producthome");
             return modelAndView;
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+            return null;
+        }
+    }
+    @RequestMapping(value = "/getAllProductsDetailsForReduxApp")
+    public List<Object> getAllProductsDetailsForReduxApp(HttpSession httpSession) {
+        List<Object> listOfProduct = new ArrayList<>();
+        List<ProductEntity> productEntities = new ArrayList<ProductEntity>();
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            productEntities = productService.getAllProductDetails();
+            System.out.println(productEntities);
+            for (ProductEntity productEntity : productEntities) {
+                listOfProduct.add(productEntity);
+            }
+            return listOfProduct;
         } catch (Exception e) {
             // TODO: handle exception
             e.printStackTrace();
