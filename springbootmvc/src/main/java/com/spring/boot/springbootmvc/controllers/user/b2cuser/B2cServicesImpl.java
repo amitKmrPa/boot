78107c9da03e966bb47b6d587949725ef5ee73b6
amitKmrPa <amit.kmr.paswan995@gmail.com>
2023-01-05 00:22:25 +0530

    remote mysql connection done

--- springbootmvc/src/main/java/com/spring/boot/springbootmvc/controllers/user/b2cuser/B2cServicesImpl.java
+++ springbootmvc/src/main/java/com/spring/boot/springbootmvc/controllers/user/b2cuser/B2cServicesImpl.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Service("b2cServices")
 public class B2cServicesImpl implements B2cServices {
@@ -63,7 +62,6 @@ public class B2cServicesImpl implements B2cServices {
             e.printStackTrace();
             msg = "User Id is available.";
             return msg;
-
         }
     }
     @Override
